f3162e43112937530f231f84a706fdb4e6ea0696
huahua <huahuazhang1988@gmail.com>
2023-02-06 13:49:28 +0100

    Initial commit

---
+++ src/main/java/com/example/spriingboot_crud1/SpriingbootCrud1Application.java
@@ -0,0 +1,13 @@
+package com.example.spriingboot_crud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriingbootCrud1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpriingbootCrud1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spriingboot_crud1/SpriingbootCrud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spriingboot_crud1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriingbootCrud1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
